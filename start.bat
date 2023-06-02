a0ddcd782df088768cad0eb5f5539442170df39c
jame windows10 <thanakornn.ch@gmail.com>
2023-06-02 22:03:00 +0700

    update requirements ,start.bat ,how to init project

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
-cd C:\Users\mrjam\OneDrive\เดสก์ท็อป\Jame\code\tools_code_memo
-echo C:\Users\mrjam\OneDrive\เดสก์ท็อป\Jame\code\tools_code_memo\env_code_memo\Scripts\activate.bat
+cd C:\Users\mrjam\OneDrive\เดสก์ท็อป\Code\Myproject\my_memo
+echo C:\Users\mrjam\OneDrive\เดสก์ท็อป\Code\Myproject\my_memo\env\Scripts\activate.bat
 echo python tools.py
 cmd /k 
